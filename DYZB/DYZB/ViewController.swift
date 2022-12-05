119c95f225c7acbaf8ec99ddd316e22aca59c329
tangtangmang00 <328311551@qq.com>
2022-12-05 16:20:04 +0800

    初始化信息

---
+++ DYZB/DYZB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DYZB
+//
+//  Created by bobo on 5/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
